f2cd674ed6fa4d3e76e160bd8df9e998474abb05
Zahidul Islam <32110153+zahidul1994@users.noreply.github.com>
2020-05-01 21:07:55 -0700

    fastapiproject
    myfastapi project

---
+++ static/FontStyle/js/custom.js
@@ -0,0 +1,33 @@
+$("#slider-range").slider({
+    range: true,
+    min: 50000,
+    max: 650000,
+    values: [75, 300],
+    slide: function (event, ui) {
+        $("#amount").val("BDT " + ui.values[0] + " - BDT " + ui.values[1]);
+    }
+});
+$("#amount").val("BDT " + $("#slider-range").slider("values", 0) +
+    " - BDT " + $("#slider-range").slider("values", 1));
+
+
+
+$(".ask-form").click(function () {
+    $(".ask-form").addClass("form-active")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
